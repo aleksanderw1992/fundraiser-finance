0c627240aa952b95931e11fb64e711f38096e8ae
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-09-21 00:50:31 +0200

    Try to compile

--- src/Badge.sol
+++ src/Badge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
 
 ///@notice Nft received for successful participation in charity
