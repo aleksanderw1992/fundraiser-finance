4235172b09dfada29e46b8582dc2e38f15e15495
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-09-23 18:22:56 +0200

    Tests WIP

--- test/CharityFactory.t.sol
+++ test/CharityFactory.t.sol
@@ -9,6 +9,7 @@ import "chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
 
 contract CharityFactoryTest is Test {
     CharityFactory contractUnderTests;
+    Badge badge;
     uint256 ETH_MIN_PRICE = 0.01 ether;
     uint256 ETH_NOT_SUFFICIENT_PRICE = 0.009 ether;
     address deployer = address(0x1223);
@@ -26,12 +27,13 @@ contract CharityFactoryTest is Test {
         musdc.mint(contributorAddress, 20000);
         vm.startPrank(deployer);
         contractUnderTests = new CharityFactory(address(musdc), address(mockChainlinkAggregator));
+        badge = new Badge(address(contractUnderTests));
         vm.stopPrank();
         vm.warp(creationTimestamp);
     }
 
     // goal is either 10 ETH (0.01 already raised) or 20 000 USDC (20 USDC already raised)
-    function createDefaultCharityTenEthGoal() public returns(uint256){
+    function createDefaultCharityTenEthGoal() public returns(uint256) {
         return contractUnderTests.createCharity{value : ETH_MIN_PRICE}
         (CharityFactory.Currency.ETH, 10, charityDefaultEndTimestamp, "successful fundraising", beneficiary);
     }
@@ -64,10 +66,50 @@ contract CharityFactoryTest is Test {
         (CharityFactory.Currency.ETH, 10, creationTimestamp - 1000, "successful fundraising", beneficiary);
     }
     
-    function testCreateCharityFailGoalTooLowInUSDC() public {
+    function testCreateCharityFailGoalTooLowInUsdc() public {
         vm.expectRevert("Cannot create a charity with too low goal");
         contractUnderTests.createCharity{value : ETH_MIN_PRICE}
         (CharityFactory.Currency.USDC, 1, charityDefaultEndTimestamp, "successful fundraising", beneficiary);
+    }
+    
+    function testCannotDonateAfterTimePass() public {
+        vm.startPrank(contributorAddress);
+        uint256 charityId = createDefaultCharityTenEthGoal();
+        vm.warp(charityDefaultEndTimestamp);
+        vm.expectRevert("Cannot donate to closed charity");
+        contractUnderTests.donateEth{value: 10 ether}(charityId);
+        vm.stopPrank();
+    }
+    
+    
+    function createAndCloseCharityGoalMet() public returns(uint256 charityId) {
+        vm.startPrank(contributorAddress);
+        charityId = createDefaultCharityTenEthGoal();
+        contractUnderTests.donateEth{value: 10 ether}(charityId);
+        vm.warp(charityDefaultEndTimestamp);
+        contractUnderTests.tryCloseCharity(charityId);
+        vm.stopPrank();
+    }
+    
+    function createAndCloseCharityGoalNotMet() public returns(uint256 charityId) {
+        vm.startPrank(contributorAddress);
+        charityId = createDefaultCharityTenEthGoal();
+        contractUnderTests.donateEth{value: 1 ether}(charityId);
+        vm.warp(charityDefaultEndTimestamp);
+        contractUnderTests.tryCloseCharity(charityId);
+        vm.stopPrank();
+    }
+    
+    function testCreateAndCloseCharityGoalMet() public {
+        uint256 charityId = createAndCloseCharityGoalMet();
+        (,,,,,,CharityFactory.CharityStatus status,,) = contractUnderTests.charities(charityId);
+        assertTrue(status == CharityFactory.CharityStatus.CLOSED_GOAL_MET);
+    }
+    
+    function testCreateAndCloseCharityGoalNotMet() public {
+        uint256 charityId = createAndCloseCharityGoalMet();
+        (,,,,,,CharityFactory.CharityStatus status,,) = contractUnderTests.charities(charityId);
+        assertTrue(status == CharityFactory.CharityStatus.CLOSED_GOAL_NOT_MET);
     }
     
     function testDonateEthAndCheckEthRaisedValues() public {
@@ -86,6 +128,26 @@ contract CharityFactoryTest is Test {
 
         assertEq(ethRaisedAfterChange, 10.01 ether);
         assertEq(usdcRaisedAfterChange, 0);
+        vm.stopPrank();
+    }
+    
+    function testDonateUsdcAndCheckUsdcRaisedValues() public {
+        vm.startPrank(contributorAddress);
+        uint256 charityId = createDefaultCharityTenEthGoal();
+        (,,,,,,CharityFactory.CharityStatus status, uint256 ethRaised, uint256 usdcRaised) = contractUnderTests.charities(charityId);
+        assertTrue(status == CharityFactory.CharityStatus.ONGOING);
+        assertEq(ethRaised, 0.01 ether);
+        assertEq(usdcRaised, 0);
+    
+        musdc.approve(address(contractUnderTests), 20000);
+        contractUnderTests.donateUsdc(charityId, 20000);
+    
+        (,,,,,,, uint256 ethRaisedAfterChange, uint256 usdcRaisedAfterChange) = contractUnderTests.charities(charityId);
+        console.log("ethRaisedAfterChange: %s", ethRaisedAfterChange);
+        console.log("usdcRaisedAfterChange: %s", usdcRaisedAfterChange);
+
+        assertEq(ethRaisedAfterChange, 0.01 ether);
+        assertEq(usdcRaisedAfterChange, 20000);
         vm.stopPrank();
     }
     
@@ -113,6 +175,7 @@ contract CharityFactoryTest is Test {
         uint256 charityId = createDefaultCharityTenEthGoal();
         musdc.approve(address(contractUnderTests), 20000);
         contractUnderTests.donateUsdc(charityId, 20000);
+        vm.warp(charityDefaultEndTimestamp);
 
         assertEq(contributorAddress.balance, 10.01 ether);
         assertEq(musdc.balanceOf(contributorAddress), 0);
@@ -126,9 +189,7 @@ contract CharityFactoryTest is Test {
         console.log("status: %s", uint8(status));
         vm.stopPrank();
     }
-    
-    function testDonateAndCloseCharityWithGoalMetAndReceiveNft() public {
-    }
+
     
     function testDonateAndCloseCharityWithGoalNotMetAndWithdrawContribution() public {
         vm.startPrank(contributorAddress);
@@ -136,23 +197,93 @@ contract CharityFactoryTest is Test {
         contractUnderTests.donateEth{value: 5 ether}(charityId);
         musdc.approve(address(contractUnderTests), 200);
         contractUnderTests.donateUsdc(charityId, 200);
+        vm.warp(charityDefaultEndTimestamp);
 
         assertEq(contributorAddress.balance, 5.01 ether);
         assertEq(musdc.balanceOf(contributorAddress), 19800);
     
         contractUnderTests.tryCloseCharity(charityId);
         (,,,,,,CharityFactory.CharityStatus status, uint256 ethRaised, uint256 usdcRaised) = contractUnderTests.charities(charityId);
-        assertTrue(status == CharityFactory.CharityStatus.CLOSED_GOAL_MET);
+        assertTrue(status == CharityFactory.CharityStatus.CLOSED_GOAL_NOT_MET);
         assertEq(ethRaised, 5.01 ether);
         assertEq(usdcRaised, 200);
     
         console.log("status: %s", uint8(status));
-//        contractUnderTests.withdrawContribution(charityId);
+        contractUnderTests.withdrawContribution(charityId);
     
-//        assertEq(contributorAddress.balance, 10.01 ether);
-//        assertEq(musdc.balanceOf(contributorAddress), 20000);
+        assertEq(contributorAddress.balance, 10.02 ether);
+        assertEq(musdc.balanceOf(contributorAddress), 20000);
         
         vm.stopPrank();
     }
+    
+    
+    function testCannotReceiveNftForGoalNotMet() public {
+        uint256 charityId = createAndCloseCharityGoalNotMet();
+        vm.prank(contributorAddress);
+        vm.expectRevert("Nft reception is possible only for closed charities with goal met");
+        contractUnderTests.receiveNtf(charityId);
+    }
+    
+    
+    function testReceiveNftForGoalMet() public {
+        uint256 charityId = createAndCloseCharityGoalMet();
+        vm.prank(contributorAddress);
+        contractUnderTests.receiveNtf(charityId);
+        
+        
+        //verify badge is visible for everybody
+        vm.prank(anotherAddress);
+        (
+        uint256 nftTokenId,
+        uint256 nftCharityId,
+        uint256 nftEthRaised,
+        uint256 nftUsdcRaised,
+        address nftContributor
+        )
+        = badge.metadata(contributorAddress, charityId);
+    
+        assertEq(nftTokenId, 1);
+        assertEq(nftCharityId, charityId);
+        assertEq(nftEthRaised, 10.1 ether);
+        assertEq(nftUsdcRaised, 0);
+        assertEq(nftContributor, contributorAddress);
+    }
+    
+    function testCannotReceiveNftTwice() public {
+        uint256 charityId = createAndCloseCharityGoalMet();
+        vm.prank(contributorAddress);
+        contractUnderTests.receiveNtf(charityId);
+        vm.expectRevert("Nft already received for user");
+        contractUnderTests.receiveNtf(charityId);
+    }
+    
+    function testCannotWithdrawFundsForGoalMet() public {
+        uint256 charityId = createAndCloseCharityGoalMet();
+        vm.prank(contributorAddress);
+        vm.expectRevert("The withdrawal of contribution is possible only for closed charities with goal not being met");
+        contractUnderTests.withdrawContribution(charityId);
+    }
+    
+
+    function testFundsTransferredBeneficiary() public {
+        vm.deal(beneficiary, 0);
+        vm.startPrank(contributorAddress);
+        uint256 charityId = createDefaultCharityTenEthGoal();
+        contractUnderTests.donateEth{value: 10 ether}(charityId);
+        musdc.approve(address(contractUnderTests), 200);
+        contractUnderTests.donateUsdc(charityId, 200);
+        vm.warp(charityDefaultEndTimestamp);
+    
+        assertEq(beneficiary.balance, 0);
+        assertEq(musdc.balanceOf(beneficiary), 0);
+        contractUnderTests.tryCloseCharity(charityId);
+    
+        assertEq(beneficiary.balance, 10.1 ether);
+        assertEq(musdc.balanceOf(beneficiary), 200);
+        vm.stopPrank();
+    }
+    
+
     
 }
