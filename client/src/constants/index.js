9b7069c862eb140555e9f783b06cf17a91ce438e
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-09-28 21:15:26 +0200

    Add constants

---
+++ client/src/constants/index.js
@@ -0,0 +1,17 @@
+const charityFactoryAddress = require("./address/charityFactoryAddress.json");
+const charityFactoryAbi = require("./abi/charityFactoryAbi.json");
+
+const badgeAddress = require("./address/badgeAddress.json");
+const badgeAbi = require("./abi/badgeAbi.json");
+
+const usdcAddress = require("./address/usdcAddress.json");
+const usdcAbi = require("./abi/usdcAbi.json");
+
+module.exports = {
+  charityFactoryAddress,
+  charityFactoryAbi,
+  badgeAddress,
+  badgeAbi,
+  usdcAddress,
+  usdcAbi,
+};
